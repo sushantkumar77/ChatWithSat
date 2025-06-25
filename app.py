49007a8ea00163bdb43acdbb8ff24efb93f171b8
SUSHANT KUMAR <135424452+sushantkumar77@users.noreply.github.com>
2025-06-25 15:45:25 +0530

    app.py

--- app.py
+++ app.py
@@ -441,7 +441,7 @@ if os.path.exists(default_pdf_path):
     pdf_input_from_user = DummyFile()
 
     if initialize_vector_db(pdf_input_from_user):
-        st.success("Hi there! 👋 How can I assist you today? Feel free to ask me anything — I’m here to help You!")
+        st.success("Hi there! 👋 How can I assist you today? Feel free to ask me anything About Us — I’m here to help You!")
 else:
     st.error(f"PDF file '{default_pdf_path}' not found in the project directory.")
 
